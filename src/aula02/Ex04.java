24d116ed6a0742b47a54e70116d1166c247f7384
Chemical-dumbneer <129759912+Chemical-dumbneer@users.noreply.github.com>
2024-10-14 12:37:15 -0300

    atualizando

--- src/aula02/Ex04.java
+++ src/aula02/Ex04.java
@@ -1,6 +1,15 @@
 package aula02;
 
-// exercicio extra: pegar enunciado
+/*
+Desenvolva um sistema que receberá o valor total da 
+compra.
+Se o valor for maior ou igual a R$50,00, calcule um 
+desconto de 10%. Caso contrário, calcule um desconto de 
+5%.
+Apresente o valor de desconto em tela.
+Utilize o conceito de operador ternário para resolução do 
+exercício.
+*/
 
 import java.util.Scanner;
 
@@ -8,6 +17,17 @@ public class Ex04 {
 
 	public static void main(String[] args) {
 		
-	}	
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Insira o valor total da compra:");
+		double valor = input.nextDouble();
+		double valFinal = valor * (1-((valor >= 50)?0.1:0.05));
+		
+		System.out.println("\nValor da compra: R$" + String.format("%.2f",valor));
+		System.out.println("Valor do Desconto: " + String.format("%.2f", ((valor >= 50)?10:5)) + "%");
+		
+
+		input.close();
+	}
 	
 }
